e0ab6f6cafc931523eeca9f75227a6dcc62aa679
Izabela Cristina <izabelacfw@gmail.com>
2024-11-12 12:31:17 -0300

    Update and rename Exercicio 2 - SQL.txt to Exercicio 2 - SQL.sql

--- 1º SEMESTRE/BANCO DE DADOS I/ATIVIDADES/Exercicio 2 - SQL/Exercicio 2 - SQL.sql
+++ 1º SEMESTRE/BANCO DE DADOS I/ATIVIDADES/Exercicio 2 - SQL/Exercicio 2 - SQL.sql
@@ -1,11 +1,13 @@
-1- Cliente (CPF PK, Cli_nome, Rua, Bairro, Cidade, Numero)
+--1 
+Cliente (CPF PK, Cli_nome, Rua, Bairro, Cidade, Numero)
 Cli_telefone (CPF* PK, Telefone PK)
 Animal (Ani_ID PK, Ani_nome, Especie, CPF*)
 Veterinario (CRV PK, Vet_nome, Salario, Experiencia)
 Enfermidade (Enf_ID PK, Gravidade, Enfi_descricao)
 Consulta (Cod_consulta PK, Data, Valor, Ani_ID*, CRV*, Enf_ID*)
 
-2- CREATE TABLE CLIENTE(
+--2 
+CREATE TABLE CLIENTE(
 CPF CHAR(11) PRIMARY KEY,
 cli_nome VARCHAR(50) NOT NULL,
 rua VARCHAR (50) NOT NULL,
@@ -53,7 +55,8 @@ FOREIGN KEY (CRV) REFERENCES Veterinario (CRV),
 FOREIGN KEY (Enf_ID) REFERENCES Enfermidade(Enf_ID)
 );
 
-3- INSERT INTO cliente
+ --3
+INSERT INTO cliente
 values
 ('11111111111','Ellie','Benedito Valadares','Centro','Pará de Minas','789'),
 ('22222222222','Joel Miller','Manuel Batista','Santos Dumont','Pará de Minas','140'),
@@ -66,13 +69,15 @@ values
 ('37966666666','22222222222'),
 ('37955555555','33333333333');
 
-4- INSERT INTO Veterinario
+--4 
+INSERT INTO Veterinario
 VALUES
 (75689,'Fernando Maciel','5500.00','3 Anos de experiencia'),
 (65893,'Anderson Tres','8000.00','5 Anos de experiencia'),
 (68712,'Isac Cardoso',7555.00,'7 Anos de experiencia');
 
-5- INSERT INTO Animal
+--5 
+INSERT INTO Animal
 VALUES
 (1,'Rex','Cachorro','11111111111'),
 (2,'Miau','Gato','22222222222'),
@@ -80,14 +85,16 @@ VALUES
 (4,'Rajado','Gato','22222222222'),
 (5,'Tobi','Cachorro','11111111111');
 
-6- INSERT INTO Enfermidade
+--6 
+INSERT INTO Enfermidade
 VALUES
 (1,'leve','desidrataçaão'),
 (2,'médio','fratura'),
 (3,'leve','corte'),
 (4,'grave','convulsão');
 
-7- INSERT INTO Consulta
+--7 
+INSERT INTO Consulta
 VALUES
 (1,'01/05/2023',300.00,1,75689,1),
 (2,'04/03/2023',250.00,2,68712,3),
@@ -95,9 +102,11 @@ VALUES
 (4,'02/04/2023',350.00,4,68712,2),
 (5,'02/04/2023',350.00,5,75689,4);
 
-8- ALTER TABLE Enfermidade ADD COLUMN tratamento VARCHAR (20);
+--8 
+ALTER TABLE Enfermidade ADD COLUMN tratamento VARCHAR (20);
 
-9- UPDATE Enfermidade
+--9 
+UPDATE Enfermidade
 SET tratamento = 'soro'
 WHERE enf_id = 1
 
@@ -113,80 +122,102 @@ UPDATE Enfermidade
 SET tratamento = 'remedio'
 WHERE enf_id = 4
 
-10- ALTER TABLE Animal RENAME COLUMN Ani_nome TO Nome
+--10 
+ALTER TABLE Animal RENAME COLUMN Ani_nome TO Nome
 
-11- UPDATE Consulta
+--11 
+UPDATE Consulta
 SET DATA = '12/01/2022'
 WHERE cod_consulta = 1
 
-12- UPDATE Veterinario
+--12 
+UPDATE Veterinario
 SET SALARIO=SALARIO+500.00
 
-13- UPDATE CONSULTA
+--13
+UPDATE CONSULTA
 SET VALOR=VALOR-20.00
 
-14- UPDATE VETERINARIO
+--14 
+UPDATE VETERINARIO
 SET SALARIO=SALARIO-50
 WHERE CRV IN (65893,68712)
 
-15- SELECT cli_nome
+--15
+SELECT cli_nome
 FROM cliente
 ORDER BY CLI_NOME;
 
-16- SELECT vet_nome
+--16
+SELECT vet_nome
 FROM veterinario
 ORDER BY vet_nome;
 
-17- SELECT nome
+--17
+SELECT nome
 FROM animal
 ORDER BY nome desc
 
-18- SELECT CRV,
+--18
+SELECT CRV,
 COUNT (1)FROM CONSULTA
 GROUP BY CRV;
 
-20- SELECT SUM(salario)
+--20 
+SELECT SUM(salario)
 FROM Veterinario
 
-21- SELECT vet_nome,salario
+--21 
+SELECT vet_nome,salario
 from veterinario
 order by salario desc
 
-23- SELECT AVG(Salario)
+--23 
+SELECT AVG(Salario)
 FROM VETERINARIO
 
-24- SELECT vet_nome,salario
+--24 
+SELECT vet_nome,salario
 FROM veterinario
 ORDER BY salario DESC
 LIMIT 1
 
-25- SELECT vet_nome,salario
+--25 
+SELECT vet_nome,salario
 FROM veterinario
 ORDER BY salario ASC
 LIMIT 1
 
-30- DELETE FROM Consulta
+--30 
+DELETE FROM Consulta
 WHERE ani_ID IN (2,4)
 
-31- DELETE FROM Veterinario
+--31 
+DELETE FROM Veterinario
 WHERE CRV = 68712
 
-32- DELETE FROM cli_telefone
+--32 
+DELETE FROM cli_telefone
 WHERE CPF ='22222222222'
 DELETE FROM Animal
 WHERE Ani_ID IN(2,4)
 DELETE FROM cliente
 WHERE cpf ='22222222222'
 
-33- DELETE FROM CONSULTA
+--33 
+DELETE FROM CONSULTA
 
-34- DROP TABLE CONSULTA
+--34
+DROP TABLE CONSULTA
 
-35- DELETE FROM VETERINARIO
+--35 
+DELETE FROM VETERINARIO
 
-36- DROP TABLE VETERINARIO
+--36
+DROP TABLE VETERINARIO
 
-37- DROP TABLE ANIMAL;
+--37 
+DROP TABLE ANIMAL;
 
 DROP TABLE CLI_TELEFONE;
 
