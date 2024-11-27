c01fdcc88a9062322ab5ab36884e3c0a40f1a876
Izabela Cristina <izabelacfw@gmail.com>
2024-11-27 15:24:43 -0300

    atualização exercicio php
    aula 12 exercicios php

---
+++ 2º SEMESTRE/PROGRAMAÇÃO WEB/PHP/Atividade aula 12/portalfilmes/enviaMensagem.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title> Portal Filmes de terror | Home </title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" type="text/css" href="style.css" media="screen"/>
+    </head>
+    <body>  
+        <section class="banner">
+            <h2 class="tituloPortal"> PORTAL FILMES DE TERROR</h2>
+        </section>
+            <div class="navMenu">
+                <a href= "index.php" class="active">Home</a>
+                <a href= "categoriasNoticias.php?Categoria=Destaques">Destaques</a>
+                <a href= "categoriasNoticias.php?Categoria=Slasher"> Slasher </a>
+                <a href= "categoriasNoticias.php?Categoria=Sobrenatural"> Sobrenatural</a>
+				<a href= "categoriasNoticias.php?Categoria=Psicologico"> Psicológico</a>
+                <a href= "categoriasNoticias.php?Categoria=Rock">Rock </a>
+                <a href= "contato.php"> Contato</a>
+                <a href= "https://www.instagram.com/izabela.cf">Sobre mim</a>
+            </div>
+		<section>
+			<div class="lerNoticia">
+				<div class="row">
+					<div class="tituloLerNoticia">
+						<h1>Mensagem Enviada!</h1>
+						<p class="text-center">Muito obrigado pela sua mensagem, assim que possível lhe retornaremos!</p>
+					</div>
+				</div>
+				
+			</div>
+		</section>
+			
+		<footer class="rodape_pagina">
+			<p>&copy; 2021 - Portal da Cidade</p>
+		</footer>
+	</body>
+</html>
